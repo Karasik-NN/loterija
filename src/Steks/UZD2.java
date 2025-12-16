53d16e435626a7c6f08e9f0414173aa50da1ff7f
Karasik-NN <nikita.nikodimovs@sk.lvt.lv>
2025-12-16 09:01:01 +0200

    Tagad ir randoms

--- src/Steks/UZD2.java
+++ src/Steks/UZD2.java
@@ -1,9 +1,18 @@
 package Steks;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class UZD2 {
+private static Stack<Integer>steks = new Stack<>();
+private static int virMin = 0;
+private static int izlMin = -1;
 
 	public static void main(String[] args) {
-
+ Random rand = new Random();
+ for(int i = 0; i < 10; i++) {
+	 
+ }
 	}
 
 }
